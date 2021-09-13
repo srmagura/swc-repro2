0663056d5322237bc0fbb85cd7fe1c07f4e5b98a
Sam Magura <srmagura@gmail.com>
2021-09-13 10:21:52 -0400

    remove some commented code

--- package-a/index.ts
+++ package-a/index.ts
@@ -1,8 +1,1 @@
-﻿export const testVariable = 'foobar'
-
-// export * from './CancellablePromise'
-// export * from './ItiReactCoreContext'
-// export * from './_Util'
-// export * from './Hooks'
-// export * from './Validation'
-// export * from './IError'
+﻿export const testVariable = "foobar";
