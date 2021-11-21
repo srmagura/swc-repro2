edc66cc4eedefe56217cd31e23a4222c896fc29f
Sam Magura <srmagura@gmail.com>
2021-11-20 22:27:25 -0500

    Run jest from root of repository

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 ﻿module.exports = {
-  transformIgnorePatterns: ["node_modules/(?!package-a)"],
+  //transformIgnorePatterns: ["node_modules/(?!package-a)"],
   transform: { "^.+\\.(t|j)sx?$": ["@swc/jest"] },
 
   testMatch: ["**/*.test.ts?(x)"],
